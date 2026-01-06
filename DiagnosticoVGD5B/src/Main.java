ab0923988f3eea0db657ff6bf4a117dbaddefe48
Vixo-vDev <20243ds046@utez.edu.mx>
2026-01-06 08:39:29 -0600

    Add files via upload
    diagnostico final

---
+++ DiagnosticoVGD5B/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner read = new Scanner(System.in);
+        int numero, suma = 0;
+
+        //Variable como bandera
+        boolean bandera = true;
+
+        while(bandera){
+            System.out.println("Ingresa un número: ");
+            numero = read.nextInt();
+            if(numero == 0){
+                break;
+            }
+
+            System.out.println("Se ha ingresado el número: "+ numero);
+            suma = suma + numero;
+
+        }
+
+        System.out.println("El resultado de la sumatoria es: "+ suma);
+    }
+}
