1c335cf200141f8e3b327f705855f75ec9cef527
Vixo-vDev <20243ds046@utez.edu.mx>
2026-01-06 08:54:39 -0600

    Agregando arrayList para mostrar los números

--- DiagnosticoVGD5B/src/Main.java
+++ DiagnosticoVGD5B/src/Main.java
@@ -1,22 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        List<Integer> numeros = new ArrayList<>();
         Scanner read = new Scanner(System.in);
         int numero, suma = 0;
 
 
         while(true){
+            System.out.println("");
             System.out.println("Ingresa un número: ");
             numero = read.nextInt();
+            numeros.add(numero);
             if(numero == 0){
                 break;
             }
 
-            System.out.println("Se ha ingresado el número: "+ numero);
+
+            for(int i = 0; i < numeros.size(); i++){
+                System.out.print(numeros.get(i) +",");
+            }
+
             suma += numero;
 
         }
